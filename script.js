b2114d13d02ecc487cbd39724879e0408bb8fabf
Himanshu Sharma <47203633+HimanshuSrma@users.noreply.github.com>
2022-11-03 15:01:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var config = {
+    // messagingSenderId: "YOUR-SENDER-ID",
+    // apiKey: "YOUR_API_KEY",
+    // projectId: "YOUR_PROJECT_ID",
+    // appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCbsozMPrFzPiQpjjEdzMuAp4JyIggL6AU",
+    authDomain: "webpushnotification-1c5d5.firebaseapp.com",
+    databaseURL: "https://webpushnotification-1c5d5-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "webpushnotification-1c5d5",
+    storageBucket: "webpushnotification-1c5d5.appspot.com",
+    messagingSenderId: "884952258335",
+    appId: "1:884952258335:web:256ef4ad5eb548cc44422a"
+};
+firebase.initializeApp(config);
